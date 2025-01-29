b66e7c9c9ae723f8a4ea1b01b610e656cb0f5655
Shubham242G <allhailvicky242@gmail.com>
2025-01-29 12:43:27 +0530

    vendor

--- src/helpers/fileSystem.ts
+++ src/helpers/fileSystem.ts
@@ -1,16 +1,174 @@
 import fs from "fs";
+import path from "path";
+import https from 'https';
+import http from 'http';
+import url from 'url';
 
-export const storeFileAndReturnNameBase64 = async (base64: string) => {
-  const tempBase64 = base64.split(",");
-  const extension = tempBase64[0].split("/")[1];
-  const filename = new Date().getTime() + `.${extension.split(";")[0]}`;
-  return new Promise((resolve, reject) => {
-    fs.writeFile(`./public/uploads/${filename}`, tempBase64[1], "base64", (err) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      }
-      resolve(filename);
+export const storeFileAndReturnNameBase64 = async (base64: string, includesFileName: boolean = false) => {
+    console.log(base64.slice(0,200), "herr 0")
+    let tempBase64 = base64.split("base64,");
+    console.log(tempBase64.slice(0,200), "tempBase64 0")
+
+    let extension = tempBase64[0].split("/")[1];
+    let filename = new Date().getTime() + `.${extension.split(";")[0]}`;
+
+    if (includesFileName) {
+        let { newFilename } = getNewFileName(`./public/uploads/${base64.split("@@")[0]}`);
+        console.log(base64.slice(0,200), "herr")
+        tempBase64 = base64.split("@@")[1].split(",");
+        filename = newFilename;
+    }
+    return new Promise((resolve, reject) => {
+        fs.writeFile(`./public/uploads/${filename}`, tempBase64[1], "base64", (err) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            }
+            resolve(filename);
+        });
     });
-  });
 };
+
+
+export const storeFileAndReturnNameBase64ForExeal = async (base64Data: string) => {
+    if (!base64Data) {
+        throw new Error("Invalid base64 data");
+    }
+
+    // Extract the actual base64 string if it's prefixed with "data:image/png;base64," etc.
+    const matches = base64Data.match(/^data:([A-Za-z-+/]+);base64,(.+)$/);
+    if (!matches || matches.length !== 3) {
+        throw new Error("Invalid base64 string format");
+    }
+
+    const buffer = Buffer.from(matches[2], 'base64');
+    const fileName = `image_${Date.now()}.png`; // Change extension if necessary
+    const filePath = path.join(__dirname, '..', 'uploads', fileName);
+
+    await fs.promises.writeFile(filePath, buffer);
+    return fileName;
+};
+
+function fileExists(filePath: string) {
+    return fs.existsSync(filePath);
+}
+/**
+ * ----------------------------------------------------------------------------
+ * Function: getNewFileName
+ * ----------------------------------------------------------------------------
+ * Description:
+ * ----------------------------------------------------------------------------
+ * Generates a new file path with a unique filename if the original filename
+ * already exists. It appends a numerical suffix to the base name of the file.
+ *
+ * ----------------------------------------------------------------------------
+ * Parameters:
+ * ----------------------------------------------------------------------------
+ * @param {string} originalPath - The original file path.
+ *
+ * ----------------------------------------------------------------------------
+ * Returns:
+ * ----------------------------------------------------------------------------
+ * @returns {Object} - An object containing the new file path and new filename:
+ *                     { newFilePath: string, newFilename: string }
+ *
+ * ----------------------------------------------------------------------------
+ * Example:
+ * ----------------------------------------------------------------------------
+ * const originalPath = './public/uploads/example.txt';
+ * const { newFilePath, newFilename } = getNewFileName(originalPath);
+ */
+function getNewFileName(originalPath: string) {
+    const dir = path.dirname(originalPath);
+    const ext = path.extname(originalPath);
+    const baseName = path.basename(originalPath, ext);
+
+    let newFilePath = originalPath;
+    let counter = 1;
+    let newFilename = `${baseName}${ext}`;
+
+    while (fileExists(newFilePath)) {
+        newFilePath = path.join(dir, `${baseName} (${counter})${ext}`);
+        newFilename = `${baseName} (${counter})${ext}`;
+        counter++;
+    }
+
+    return { newFilePath, newFilename };
+}
+
+
+export const deleteFileUsingUrl = (url: string) => {
+    try {
+        if (fileExists("./public" + "/" + url)) {
+            fs.unlinkSync("./public" + "/" + url)
+        }
+    }
+    catch (err) {
+        return err
+    }
+}
+
+
+
+export async function downloadFile(fileUrl: string) {
+    // Parse the URL to get the filename
+    const parsedUrl: any = url.parse(fileUrl);
+    const filename = path.basename(parsedUrl.pathname);
+
+    // Define the file path
+    const filePath = path.join(process.cwd(), "public", "uploads", filename);
+
+    // Make the request and download the file
+    const fileStream: any = await new Promise((resolve, reject) => {
+        const request = https.get(fileUrl, (response: any) => {
+            if (response.statusCode !== 200) {
+                reject(new Error(`Failed to get file from ${fileUrl}, status code: ${response.statusCode}`));
+                return;
+            }
+
+            // Create a writable stream
+            const stream = fs.createWriteStream(filePath);
+            response.pipe(stream);
+
+            stream.on('finish', () => resolve(stream));
+            stream.on('error', reject);
+        });
+
+        request.on('error', reject);
+    });
+
+    await fileStream.close(); // Ensure the stream is closed
+
+    return filename;
+}
+
+
+
+export const downloadFileHttp = (url: string, filePath: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      const protocol = url.startsWith('https') ? https : http;
+  
+      protocol.get(url, (response: any) => {
+        if (response.statusCode === 200) {
+          const file = fs.createWriteStream(filePath);
+          response.pipe(file);
+          file.on('finish', () => {
+            file.close();
+            resolve(filePath); // Return the final file path
+          });
+        } else if (response.statusCode === 301 || response.statusCode === 302) {
+          // Follow redirect if the response code is 301 or 302
+          const newUrl = response.headers.location;
+          downloadFileHttp(newUrl, filePath)
+            .then(resolve)
+            .catch(reject);
+        } else {
+          reject(new Error(`Failed to download file: ${response.statusCode}`));
+        }
+      }).on('error', (err) => {
+        console.error(`Error: ${err.message}`);
+        reject(err);
+      });
+    });
+  };
+  
--- src/models/vendor.model.ts
+++ src/models/vendor.model.ts
@@ -1,263 +1,247 @@
 import { model, Model, Schema, Types } from "mongoose";
 
 interface IVendor {
-  _id: Types.ObjectId;
-  contactName: string;
-  companyName: string;
-  displayName: string;
-  salutation: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  landLine : string;
-  phoneNumber: string;
-  mobileNumber: string;
-  paymentTerms: string;
-  currencyCode: string;
-  notes: string;
-  Website: string;
-  status: string;
-  openingBalance: string;
-  branchId: string;
-  branchName: string;
-  paymentTermsLabel: string;
-  sourceOfSupply: string;
-  skypeIdentity: string;
-  department: string;
-  designation: string;
+  // contactName: string;
+    _id: Types.ObjectId;
+    firstName: string;
+    lastName: string;
+    email: string;
+    landLine : string;
+    phoneNumber: string;
+    displayName: string;
+    
+    otherDetails:{
+    sourceOfSupply: string;
+    gstTreatment: string;
+    gstin: string;
+    pan: string;
+    msmeRegistered: boolean;
+    currency: string;
+    paymentTerms: string;
+    tds: string;
+    priceList: string;
+    enablePortal: boolean;
+    portalLanguage: string;
+    documents?:string;
+    addOtherDetails: [];
+    };
 
-  // social media
-  facebook: string;
-  twitter: string;
+    billingAddress:{
+      addressId: Types.ObjectId;
+      attention : string;
+      billingCountry: string;
+      billingAddressStreet1: string;
+      billingAddressStreet2: string;
+      billingCity: string;
+      billingState: string;
+      billingPincode: string;
+      billingPhone: string;
+      billingFaxNumber:string;
+    };
 
-  // gst
-  gstTreatment: string;
-  gstin: string;
-
-  // udyam
-  msmeUdyamNo: string;
-  udyamType: string;
-
-  // tds
-  tdsName: string;
-  tdsPercentage: string;
-  tdsSectionCode: string;
-  tdsSection: string;
-  priceList: string;
-
-  //address
-  contactAddressId: string;
-
-  // billing address
-  billingAttention: string;
-  billingAddress: string;
-  billingCity: string;
-  billingState: string;
-  billingCountry: string;
-  billingCode: string;
-  billingPhoneNumber: string;
-  alternateContact : string;
-  billingFax: string;
-
-  // shipping details
-
-  shippingAttentation: string;
-  shippingAddress: string;
-  shippingCity: string;
-  shippingState: string;
-  shippingCountry: string;
-  shippingCode: string;
-  shippingPhone: string;
-  shippingFax: string;
-  source: string;
-  lastSyncTime: string;
-  exchangeRate: string;
-  ownerName: string;
-  primaryContactId: string;
-  beneficiaryName: string;
-
-  // vendor details
-  vendorBankAccountNumber: string;
-  vendorBankName: string;
-  vendorIfscCode: string;
-
-  vendorBankCode: string;
-
-  //category drop down
-
-  // categoryId : Types.ObjectId,
-  // Array Elements
-  hotelArr: {
-    hotelId: Types.ObjectId;
-    roomsArr: {
-      category: string;
-      size: string;
-      price: number;
-      imagesArr: {
-        image: string;
-      }[];
+    shipppingAddress:{
+      attention : string;
+      shippingCountry: string;
+      shippingAddressStreet1: string;
+      shippingAddressStreet2: string;
+      shippingCity: string;
+      shippingState: string;
+      shippingPincode: string;
+      shippingPhone: string;
+      shippingFaxNumber:string;
+    }
+    // contactPersons
+    contactPersons:{
+      salutation: string;
+      contactPersonId: Types.ObjectId;
+      contactPersonFirstName: string;
+      contactPersonLastName: string;
+      contactPersonEmail: string;
+      contactPersonWorkPhone: string;
+      contactPersonMobile: string;
     }[];
-  }[];
+ 
+    documents?: string;
 
-  banquetArr: {
-    category: string;
-    banquetName: string;
-    size: string;
-    setup: string;
-    vegPrice: number;
-    nonVegPrice: number;
-    pfa: boolean;
-    pfaSize: string;
-    imagesArr: {
-      image: string;
-    }[];
-  }[];
+  // companyName: string;
+  // displayName: string;
+  // salutation: string;
+  // firstName: string;
+  // lastName: string;
+  
+  // mobileNumber: string;
+  // paymentTerms: string;
+  // currencyCode: string;
+  // notes: string;
+  // Website: string;
+  // status: string;
+  // openingBalance: string;
+  // branchId: string;
+  // branchName: string;
+  // paymentTermsLabel: string;
+  // sourceOfSupply: string;
+  // skypeIdentity: string;
+  // department: string;
+  // designation: string;
 
-  resturantArr: {
-    foodOptions: string;
-    noOfOccupancy: number;
-    floor: string;
-    isSwimimgPool: string;
-    imagesArr: {
-      image: string;
-    }[];
-  }[];
+  // // social media
+  // facebook: string;
+  // twitter: string;
 
-  createdAt: Date;
-  updatedAt: Date;
+ 
+
+
+
+  // // udyam
+  // msmeUdyamNo: string;
+  // udyamType: string;
+
+  // // tds
+  // tdsName: string;
+  // tdsPercentage: string;
+  // tdsSectionCode: string;
+  // tdsSection: string;
+  // priceList: string;
+  // contactPersonName: string;
+  // // billing address
+  // billingAttention: string;
+  // billingAddress: string;
+  // billingCity: string;
+  // billingState: string;
+  // billingCountry: string;
+  // billingCode: string;
+  // billingPhoneNumber: string;
+  // alternateContact : string;
+  // billingFax: string;
+
+  // // shipping details
+
+  // shippingAttentation: string;
+  // shippingAddress: string;
+  // shippingCity: string;
+  // shippingState: string;
+  // shippingCountry: string;
+  // shippingCode: string;
+  // shippingPhone: string;
+  // shippingFax: string;
+  // source: string;
+  // lastSyncTime: string;
+  // exchangeRate: string;
+  // ownerName: string;
+  // primaryContactId: string;
+  // beneficiaryName: string;
+
+  // // vendor details
+  // vendorBankAccountNumber: string;
+  // vendorBankName: string;
+  // vendorIfscCode: string;
+
+  // vendorBankCode: string;
+
+  // //category drop down
+
+  // // categoryId : Types.ObjectId,
+  // // Array Elements
+  // hotelArr: {
+  //   hotelId: Types.ObjectId;
+  //   roomsArr: {
+  //     category: string;
+  //     size: string;
+  //     price: number;
+  //     imagesArr: {
+  //       image: string;
+  //     }[];
+  //   }[];
+  // }[];
+
+  // banquetArr: {
+  //   category: string;
+  //   banquetName: string;
+  //   size: string;
+  //   setup: string;
+  //   vegPrice: number;
+  //   nonVegPrice: number;
+  //   pfa: boolean;
+  //   pfaSize: string;
+  //   imagesArr: {
+  //     image: string;
+  //   }[];
+  // }[];
+
+  // resturantArr: {
+  //   foodOptions: string;
+  //   noOfOccupancy: number;
+  //   floor: string;
+  //   isSwimimgPool: string;
+  //   imagesArr: {
+  //     image: string;
+  //   }[];
+  // }[];
+
+  // createdAt: Date;
+  // updatedAt: Date;
 }
 
 const vendorSchema = new Schema({
-  contactName: String,
-  companyName: String,
-  displayName: String,
-  salutation: String,
+  // contactName: string;
+  _id: String,
   firstName: String,
   lastName: String,
   email: String,
   landLine : String,
-  alternateContact : String,
   phoneNumber: String,
-  mobileNumber: String,
-  paymentTerms: String,
-  currencyCode: String,
-  notes: String,
-  Website: String,
-  status: String,
-  openingBalance: String,
-  branchId: String,
-  branchName: String,
-  paymentTermsLabel: String,
+  displayName: String,
+  
+  otherDetails:{
   sourceOfSupply: String,
-  skypeIdentity: String,
-  department: String,
-  designation: String,
-
-  // social media
-  facebook: String,
-  twitter: String,
-
-  // gst
   gstTreatment: String,
   gstin: String,
-
-  // udyam
-  msmeUdyamNo: String,
-  udyamType: String,
-
-  // tds
-  tdsName: String,
-  tdsPercentage: String,
-  tdsSectionCode: String,
-  tdsSection: String,
+  pan: String,
+  msmeRegistered: Boolean,
+  currency: String,
+  paymentTerms: String,
+  tds: String,
   priceList: String,
+  enablePortal: Boolean,
+  portalLanguage: String,
+  documents: String,
+  addOtherDetails: [],
+  },
 
-  //address
-  contactAddressId: String,
+  billingAddress:{
+    addressId: String,
+    attention : String,
+    billingCountry: String,
+    billingAddressStreet1: String,
+    billingAddressStreet2: String,
+    billingCity: String,
+    billingState: String,
+    billingPincode: String,
+    billingPhone: String,
+    billingFaxNumber:String,
+  },
 
-  // billing address
-  billingAttention: String,
-  billingAddress: String,
-  billingCity: String,
-  billingState: String,
-  billingCountry: String,
-  billingCode: String,
-  billingPhoneNumber: String,
-  billingFax: String,
-
-  // shipping details
-
-  shippingAttentation: String,
-  shippingAddress: String,
+  shipppingAddress:{
+  attention : String,
+  shippingCountry: String,
+  shippingAddressStreet1: String,
+  shippingAddressStreet2: String,
   shippingCity: String,
   shippingState: String,
-  shippingCountry: String,
-  shippingCode: String,
+  shippingPincode: String,
   shippingPhone: String,
-  shippingFax: String,
-  source: String,
-  lastSyncTime: String,
-  exchangeRate: String,
-  ownerName: String,
-  primaryContactId: String,
-  beneficiaryName: String,
-
-  // vendor details
-  vendorBankAccountNumber: String,
-  vendorBankName: String,
-  vendorBankCode: String,
-
-  // Array Elements
-  hotelArr: [
-    {
-      hotelId: Types.ObjectId,
-      roomsArr: [
-        {
-          category: String,
-          noOfRooms: Number,
-          size: String,
-          price: Number,
-          imagesArr: [
-            {
-              image: String,
-            },
-          ],
-        },
-      ],
-    },
-  ],
-
-  banquetArr: [
-    {
-      category: String,
-      banquetName: String,
-      size: String,
-      setup: String,
-      vegPrice: Number,
-      nonVegPrice: Number,
-      pfa: Boolean,
-      pfaSize: String,
-      imagesArr: [
-        {
-          image: String,
-        },
-      ],
-    },
-  ],
-
-  resturantArr: [
-    {
-      foodOptions: String,
-      noOfOccupancy: Number,
-      floor: String,
-      isSwimimgPool: String,
-      imagesArr: [
-        {
-          image: String,
-        },
-      ],
-    },
-  ],
+  shippingFaxNumber:String,
+  },
+  // contactPersons
+  contactPersons:[{
+    salutation: String,
+    contactPersonId: String,
+    contactPersonFirstName: String,
+    contactPersonLastName: String,
+    contactPersonEmail: String,
+    contactPersonWorkPhone: String,
+    contactPersonMobile: String,
+  }],
 });
 
 export const Vendor = model<IVendor>("vendor", vendorSchema);
